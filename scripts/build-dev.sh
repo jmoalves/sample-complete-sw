e086febcc21f04618b9b0e5863fa0918e4301510
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2023-08-18 15:06:05 +0000

    script falha ao encontrar erro

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd && cd - >/dev/null 2>&1 )"
 
 docker build \
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd && cd - >/dev/null 2>&1 )"
 
 docker run -it --rm --mount type=bind,source="$(pwd)",target=/home/developer/src jmoalves/sample-app-build "mvn clean"
